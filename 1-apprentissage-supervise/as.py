fc8646ac2657ea637546b68f459c15806f3d1bfd
he-lau <he.laurent.1130@gmail.com>
2022-10-08 17:41:45 +0200

    MAJ suite mal compréhenssion du sujet

--- 1-apprentissage-supervise/as.py
+++ 1-apprentissage-supervise/as.py
@@ -21,78 +21,67 @@ df = pd.read_csv('credit_scoring.csv')
 # Show dataframe
 #print(df.values)
 
-status0 = []
-status1 = []
 
 print("-------------- source --------------")
 
-test = np.array(df)
+source = np.array(df)
 
 
-print(test[0])
-print(test[0][0])
-print(test.size)
-print(test.shape)
+print(source[0])
+print(source[0][0])
+print(source.size)
+print(source.shape)
 
 print("Length df : " + str(len(df.values)))
 print("Value at 0 df : " + str(df.values[0][0]))
 print("Value at 0 df : " + str(len(df.values[0][0])))
 
 
-
+status = []
+caracteristiques = []
 
 
 for i in range (0,len(df.values)):
-    # print(df.values[i][0][len(df.values[i][0])-1])   
+    
     split = df.values[i][0].split(";")
 
-    if df.values[i][0][len(df.values[i][0])-1] == '0' :
-        status0.append(split)
-    else :
-        status1.append(split)
+    status.append(split[13])
 
-print("-------------- status0 --------------")
+    split.pop(13)
 
-status0 = np.array(status0)
-
-print("size : " + str(status0.size))
-print("shape : " + str(status0.shape))
-print("Premier elem status0 : " + str(status0[0]))
-
-#for i in status0 :
-    #print(i)
-    #print("\n")
-
-print("-------------- status1 --------------")
-
-status1 = np.array(status1)
-
-print("size : " + str(status1.size))
-print("shape : " + str(status1.shape))
+    caracteristiques.append(split)
 
 
 
+print("-------------- status --------------")
+
+status = np.array(status)
+
+print("size : " + str(status.size))
+print("shape : " + str(status.shape))
 
 
-status0_age = []
-status1_age = []
+print("-------------- variables caractéristiques --------------")
 
-for i in status0 :
-    status0_age.append(i[3]) 
+caracteristiques = np.array(caracteristiques)
 
-for i in status1 :
-    status1_age.append(i[3])     
+print("size : " + str(caracteristiques.size))
+print("shape : " + str(caracteristiques.shape))
 
 
-status0_age.sort()
+print(caracteristiques)
 
-for i in status0_age :
-    print(i)
+
+age = []
+
+for i in caracteristiques :
+    age.append(i[3]) 
+
+# age.sort()
+print(age)
 
 plt.style.use('ggplot')
-plt.hist(status0_age, bins=10) 
-plt.title("status0 - Age") 
-plt.xlabel("Age")
-plt.ylabel("Age Disribution")
+
+plt.hist(status) 
 
 plt.show()
